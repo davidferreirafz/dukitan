a583256b6787da50b30b4c201f6a17b212ea1784
davidferreira.FZ <davidferreira.FZ@af0da060-6b45-0410-ad08-0d77b38a8a5d>
2008-02-07 01:47:59 +0000

    Issue 01 - Corrigindo Bug - http://code.google.com/p/dukitan/issues/detail?id=1

--- trunk/CalcTicket/src/com/wordpress/dukitan/calcticket/model/DAOCompras.java
+++ trunk/CalcTicket/src/com/wordpress/dukitan/calcticket/model/DAOCompras.java
@@ -119,31 +119,30 @@ public class DAOCompras
         byte[] b = null;        
         DAOCompras dao = new DAOCompras();  
         dao.limpar();
-        dao.abrir();        
+        dao.abrir();
         
         try {
             ByteArrayOutputStream baos = null;
             DataOutputStream dos = null;
 
-            try {               
-                int tamanho = compras.size();
+            int tamanho = compras.size();
                 
+            if (tamanho>0){
                 for (int i=0; i<tamanho; i++){
                     baos = new ByteArrayOutputStream();
                     dos = new DataOutputStream(baos);
-                    
+
                     Produto p = compras.getProduto(i);
-                    
+
                     dos.writeUTF(p.getDescricao());
                     dos.writeInt(p.getQuantidade());
                     dos.writeFloat(p.getValor());
 
                     b = baos.toByteArray();
-                    
+
                     dao.gravar(b);
+                    dos.close();
                 }
-            } finally {
-               dos.close();                
             }
         } catch (IOException ex) {
             ex.printStackTrace();
@@ -165,30 +164,29 @@ public class DAOCompras
         try {            
             int tamanho = dao.size();
             
-            for (int i=0; i<tamanho; i++){
-              
-                byte[] dados = dao.ler(ID+i);
-                if (dados!=null){
-                    bais = new ByteArrayInputStream(dados);               
-                    dis = new DataInputStream(bais);
+            if (tamanho>0){
+                for (int i=0; i<tamanho; i++){
 
-                    Produto p = new Produto();
+                    byte[] dados = dao.ler(ID+i);
+                    if (dados!=null){
+                        bais = new ByteArrayInputStream(dados);               
+                        dis = new DataInputStream(bais);
 
-                    p.setDescricao(dis.readUTF());
-                    p.setQuantidade(String.valueOf(dis.readInt()));
-                    p.setValor(String.valueOf(dis.readFloat()));
+                        Produto p = new Produto();
 
-                    c.adicionar(p);
-                }                
-            }           
+                        p.setDescricao(dis.readUTF());
+                        p.setQuantidade(String.valueOf(dis.readInt()));
+                        p.setValor(String.valueOf(dis.readFloat()));
+
+                        c.adicionar(p);
+
+                        dis.close();
+                    }                
+                }           
+            }
         } catch (IOException ex) {
             ex.printStackTrace();
         } finally {
-            try {
-                if (dis!=null){
-                    dis.close();
-                }
-            } catch (IOException ex) { ex.printStackTrace(); }
             dao.fechar();
         }
         
