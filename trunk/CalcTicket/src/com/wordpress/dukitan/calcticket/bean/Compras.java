42818cc03d74349a0058b19573fd005aa3003689
davidferreira.FZ <davidferreira.FZ@af0da060-6b45-0410-ad08-0d77b38a8a5d>
2008-02-13 02:38:18 +0000

    Adicionando informações sobre a Licença

--- trunk/CalcTicket/src/com/wordpress/dukitan/calcticket/bean/Compras.java
+++ trunk/CalcTicket/src/com/wordpress/dukitan/calcticket/bean/Compras.java
@@ -1,7 +1,23 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+///***************************************************************************
+// *   CalcTicket - Mobile                                                   *
+// *   Copyright (C) 2008 by David Ferreira - FZ & DukItan Software          *
+// *   davidferreira.fz@gmail.com - http://dukitan.wordpress.com             *
+// ***************************************************************************
+// *   Este programa é software livre; você pode redistribuí-lo e/ou         *
+// *   modificá-lo sob os termos da Licença Pública Geral GNU, conforme      *
+// *   publicada pela Free Software Foundation, tanto a versão 3 da          *
+// *   Licença como (a seu critério) qualquer versão mais nova.              *
+// ***************************************************************************
+// *   This program is free software; you can redistribute it and/or modify  *
+// *   it under the terms of the GNU General Public License as published by  *
+// *   the Free Software Foundation; either version 3 of the License, or     *
+// *   (at your option) any later version.                                   *
+// *                                                                         *
+// *   You should have received a copy of the GNU General Public License     *
+// *   along with this program; if not, write to the                         *
+// *   Free Software Foundation, Inc.,                                       *
+// *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+// ***************************************************************************/
 
 package com.wordpress.dukitan.calcticket.bean;
 
