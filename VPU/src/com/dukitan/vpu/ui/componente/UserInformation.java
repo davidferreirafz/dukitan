053625c16aee24147211d7c9ea5a45bd58174ec8
davidferreira.FZ <davidferreira.FZ@af0da060-6b45-0410-ad08-0d77b38a8a5d>
2010-08-28 12:56:46 +0000

    Corrigindo informações de UI (UserInterface); Atualizando informações de Créditos; Corrigindo versão da imagem;

--- VPU/src/com/dukitan/vpu/ui/componente/UserInformation.java
+++ VPU/src/com/dukitan/vpu/ui/componente/UserInformation.java
@@ -25,7 +25,7 @@ public class UserInformation extends JPanel
 		labelDono.setText("Dono do Esquema:");
 		labelDono.setBounds(new java.awt.Rectangle(10,10,136,61));
 		labelUsuario = new JLabel();
-		labelUsuario.setText("Usu�rio do Esquema:");
+		labelUsuario.setText("Usuário do Esquema:");
 		labelUsuario.setBounds(new java.awt.Rectangle(10,40,136,61));
 		labelDiferenca = new JLabel();
 		labelDiferenca.setText("Faltando:");
@@ -34,15 +34,15 @@ public class UserInformation extends JPanel
 		donoQuantidade = new JLabel();
 		donoQuantidade.setText("00");
 		donoQuantidade.setFont(new java.awt.Font("Dialog", java.awt.Font.BOLD, 14));
-		donoQuantidade.setBounds(new java.awt.Rectangle(141,10,82,61));		
+		donoQuantidade.setBounds(new java.awt.Rectangle(150,10,82,61));		
 		usuarioQuantidade = new JLabel();
 		usuarioQuantidade.setText("00");
 		usuarioQuantidade.setFont(new java.awt.Font("Dialog", java.awt.Font.BOLD, 14));
-		usuarioQuantidade.setBounds(new java.awt.Rectangle(141,40,82,61));
+		usuarioQuantidade.setBounds(new java.awt.Rectangle(150,40,82,61));
 		diferencaQuantidade = new JLabel();
 		diferencaQuantidade.setText("00");
 		diferencaQuantidade.setFont(new java.awt.Font("Dialog", java.awt.Font.BOLD, 14));
-		diferencaQuantidade.setBounds(new java.awt.Rectangle(141,70,82,61));
+		diferencaQuantidade.setBounds(new java.awt.Rectangle(150,70,82,61));
 		
 		setLayout(null);
 		setBorder(javax.swing.BorderFactory.createTitledBorder(null, titulo, javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, null, null));
