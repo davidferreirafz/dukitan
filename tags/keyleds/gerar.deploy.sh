39636aa4c23de523b2a6e7655b0d97dd028a4229
davidferreira.FZ <davidferreira.FZ@af0da060-6b45-0410-ad08-0d77b38a8a5d>
2011-06-08 18:28:16 +0000

    KeyLEDs: Atualizando geração de distribuição: bin.zip e archlinux;

--- tags/keyleds/gerar.deploy.sh
+++ tags/keyleds/gerar.deploy.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
-export VERSAO=1.0
+export VERSAO=1.2
 export PACKAGE_NAME=keyleds.$VERSAO.bin.zip
 
-export RAIZ=/home/desenvolvimento/dukitan/
-export PROJETO=$RAIZ/trunk/KeyLEDs/
-export RELEASE=$PROJETO/bin/
-export DISTRO=$RAIZ/tags/keyleds/
+export RAIZ=/home/desenvolvimento/dukitan/tags/keyleds
+export CURRENT=$RAIZ/current/
+export DISTRO=$RAIZ/distro/
 
 echo "Criando: KeyLEDs $VERSAO"
-cd $RELEASE;
+cd $CURRENT;
 echo "    Criando pacote... (Zip - All)"
 zip -rq4 $DISTRO/$PACKAGE_NAME * -x *SVN* *svnignore* *.svn*
 echo "    Pacote Finalizado"
