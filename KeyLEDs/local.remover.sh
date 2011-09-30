25d528b92ce3f8834af6d0521a12de68cca670ce
davidferreira.fz <davidferreira.fz@af0da060-6b45-0410-ad08-0d77b38a8a5d>
2011-09-30 01:05:38 +0000

    UsherUrl: Adicionando Projeto v1.5

--- KeyLEDs/local.remover.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rm -v /usr/lib/kde4/plasma_applet_keyleds.so
-rm -v /usr/share/kde4/services/plasma-applet-keyleds.desktop
--- WebGroupTranslator/lib/saa/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-
-session_start();
-session_unregister('userTO');
-
-
-?>
