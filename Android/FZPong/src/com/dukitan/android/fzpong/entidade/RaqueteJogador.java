97d73f1891d1491e1ed78749b2ea195c21a057ec
davidferreira.fz <davidferreira.fz@af0da060-6b45-0410-ad08-0d77b38a8a5d>
2011-10-19 16:31:50 +0000

    FZPong: Criando Classe para representação de Sprite;

--- Android/FZPong/src/com/dukitan/android/fzpong/entidade/RaqueteJogador.java
+++ Android/FZPong/src/com/dukitan/android/fzpong/entidade/RaqueteJogador.java
@@ -1,13 +1,12 @@
 package com.dukitan.android.fzpong.entidade;
 
 import android.graphics.Bitmap;
-import android.graphics.Point;
 import android.graphics.Rect;
 
 public class RaqueteJogador extends Raquete {
 
-	public RaqueteJogador(Bitmap sprite) {
-		super(sprite, new Point(784,100),new Rect(0,21,14,101));
+	public RaqueteJogador(Bitmap imagem) {
+		super(new Rect(0, 21, 14, 80), imagem);
 	}
 
 }
