6cca4cc84e92c66b91f7f37c9b5a7852496b7e92
davidferreira.fz <davidferreira.fz@af0da060-6b45-0410-ad08-0d77b38a8a5d>
2011-10-24 15:49:11 +0000

    FZPong: Alterando para uso do componente SurfaceView;

--- Android/FZPong/src/com/dukitan/android/fzpong/FZPongActivity.java
+++ Android/FZPong/src/com/dukitan/android/fzpong/FZPongActivity.java
@@ -4,50 +4,101 @@ import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Message;
-import android.view.View;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import com.google.ads.AdRequest;
 import com.google.ads.AdView;
 
 public class FZPongActivity extends Activity {
-    /** Called when the activity is first created. */
 
-    GameView view;
-    AdView adView;
-    private Handler guiRefresher;
+    private static final int MENU_PAUSE = 1;
+
+    private static final int MENU_RESUME = 2;
+
+    private static final int MENU_START = 3;
+
+    private static final int MENU_STOP = 4;
+
+    private GameView view;
+    //private AdView adView;
+
+    private Controle controle;
 
     @Override
-    public void onCreate(Bundle state) {
-        super.onCreate(state);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.main);
 
         view = (GameView) findViewById(R.id.gameView);
+        controle = view.getThread();
+ 
 
-        guiRefresher = new Handler() {
-            public void handleMessage(Message msg) {
-                if (msg.what == 1) {
-                    view.invalidate();
-                }
-                super.handleMessage(msg);
-            }
-        };
-
-        view.setCallbackHandler(guiRefresher);
-
-        Thread t = new Thread(view);
-        t.setDaemon(true);
-        t.start();
-
-        adView = (AdView) this.findViewById(R.id.adView);
+        //adView = (AdView) this.findViewById(R.id.adView);
+        
+        if (savedInstanceState == null) {
+            // we were just launched: set up a new game
+            //mLunarThread.setState(LunarThread.STATE_READY);
+           // Log.w(this.getClass().getName(), "SIS is null");
+        } else {
+            // we are being restored: resume a previous game
+            //mLunarThread.restoreState(savedInstanceState);
+            //Log.w(this.getClass().getName(), "SIS is nonnull");
+        }        
 
     }
 
+    
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        menu.add(0, MENU_START, 0, R.string.menu_start);
+        menu.add(0, MENU_STOP, 0, R.string.menu_stop);
+        menu.add(0, MENU_PAUSE, 0, R.string.menu_pause);
+        menu.add(0, MENU_RESUME, 0, R.string.menu_resume);
+
+        return true;
+    }    
+    
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_START:
+                controle.doStart();
+                return true;
+            case MENU_STOP:
+                controle.setState(Controle.STATE_LOSE,
+                        getText(R.string.message_stopped));
+                return true;
+            case MENU_PAUSE:
+                controle.pause();
+                return true;
+            case MENU_RESUME:
+                controle.unpause();
+                return true;
+        }
+
+        return false;
+    }    
+    
+    @Override
+    protected void onPause() {
+        super.onPause();
+        view.getThread().pause(); // pause game when Activity pauses
+    }
+
+
+    protected void onSaveInstanceState(Bundle outState) {
+        // just have the View's thread save its state into our Bundle
+        super.onSaveInstanceState(outState);
+        controle.saveState(outState);
+        Log.w(this.getClass().getName(), "SIS called");
+    }
+    /*
     public void onDestroy() {
         // adView.destroy();
 
         super.onDestroy();
-    }
+    }*/
 
 }
