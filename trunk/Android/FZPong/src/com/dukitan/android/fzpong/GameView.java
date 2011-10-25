da4621e1413e9bf1c011a64352f7493d114062ab
davidferreira.FZ <davidferreira.FZ@af0da060-6b45-0410-ad08-0d77b38a8a5d>
2011-10-25 02:20:07 +0000

    FZPong: Corrigindo mudança de estado e exibição do AdMob;

--- trunk/Android/FZPong/src/com/dukitan/android/fzpong/GameView.java
+++ trunk/Android/FZPong/src/com/dukitan/android/fzpong/GameView.java
@@ -8,16 +8,12 @@ import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.View;
 
-public class GameView extends SurfaceView implements SurfaceHolder.Callback
-{
+public class GameView extends SurfaceView implements SurfaceHolder.Callback {
 
-    private View     adView;
     private Controle thread;
 
-    public GameView(Context context, AttributeSet attrs)
-    {
+    public GameView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
         SurfaceHolder holder = getHolder();
@@ -26,8 +22,7 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback
         // create thread only; it's started in surfaceCreated()
         thread = new Controle(holder, context, new Handler() {
             @Override
-            public void handleMessage(Message m)
-            {
+            public void handleMessage(Message m) {
                 // mStatusText.setVisibility(m.getData().getInt("viz"));
                 // mStatusText.setText(m.getData().getString("text"));
             }
@@ -37,27 +32,23 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback
 
     }
 
-
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height)
-    {
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
         thread.setSurfaceSize(width, height);
     }
 
-    public void surfaceCreated(SurfaceHolder holder)
-    {
+    public void surfaceCreated(SurfaceHolder holder) {
         thread.setRunning(true);
         thread.start();
     }
 
     @Override
-    public void onWindowFocusChanged(boolean hasWindowFocus)
-    {
-        if (!hasWindowFocus)
+    public void onWindowFocusChanged(boolean hasWindowFocus) {
+        if (!hasWindowFocus) {
             thread.pause();
+        }
     }
 
-    public void surfaceDestroyed(SurfaceHolder holder)
-    {
+    public void surfaceDestroyed(SurfaceHolder holder) {
         boolean retry = true;
         thread.setRunning(false);
         while (retry) {
@@ -70,22 +61,16 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback
     }
 
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event)
-    {
-       return thread.doKeyDown(keyCode, event);
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return thread.doKeyDown(keyCode, event);
     }
 
-    
     @Override
-    public boolean onTouchEvent(MotionEvent event)
-    {
-        thread.onTouchEvent(event);
-        
-        return true;
+    public boolean onTouchEvent(MotionEvent event) {
+        return thread.doTouchEvent(event);
     }
-    
-    public Controle getThread()
-    {
+
+    public Controle getThread() {
         return thread;
     }
 
